021093e95d2796c7a90d93ea53c206bbd1e32be9
JeevaJV <138186027+JeevaJV@users.noreply.github.com>
2023-11-28 18:46:13 +0530

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,8 @@
+"https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js">
+"https://cdnjs.cloudflare.com/ajax/libs/jquery.ripples/0.5.3/jquery.ripples.min.js">
+        $(".full-landing-image").ripples({
+            resolution: 512,
+            dropRadius: 20,
+            interactive: true,
+            perturbance: 0.5,
+        });
